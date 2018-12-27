1b5442d86802cf0a2c944c10f7264135f4c30c7b
rebelwarrior <rabbit.editor@gmail.com>
2018-12-27 00:55:05 -0400

    locks i18n to 1.1.1 and adds browser lang change

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :set_locale
+  around_action :switch_locale
 
-  def set_locale
-    # I18n.locale = params[:locale] || I18n.default_locale
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
   end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
 end
