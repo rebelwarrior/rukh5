d8d6a3a6a113ab9cf5df173fc4741b56316e1d85
rebelwarrior <rabbit.editor@gmail.com>
2019-03-01 02:45:28 -0400

    adds more tests

--- spec/helpers/import_logic2_spec.rb
+++ spec/helpers/import_logic2_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 
-# require "FindFileLines"
 require "import_logic2"
 
 describe FindFileLines do 
   # let(:csv_test_file) { File.expand_path('./spec/helpers/test.csv') }
+  let(:find_lines_obj) { FindFileLines.new }
   
   # it "calculates the number of lines in a file" do
-  #   lines = FindFileLines.new.perform(csv_test_file)
+  #   lines = find_lines_obj.perform(csv_test_file)
   #   expect(lines).to eq(3)
   # end
   
   it "calculates the number of lines in a stringIO / opened file" do 
     opened_file = StringIO.new("1\n2\n")
-    lines = FindFileLines.new.find_number_lines(opened_file)
+    lines = find_lines_obj.find_number_lines(opened_file)
     expect(lines).to eq(2)
     
   end
